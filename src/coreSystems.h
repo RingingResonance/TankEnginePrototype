a667fe4e57442a5ea8a3291135f593d18225b626
perpetualwalnut <madeofsolder@gmail.com>
2021-07-13 18:42:50 -0500

    Initial Commit

---
+++ src/coreSystems.h
@@ -0,0 +1,37 @@
+
+
+#ifndef renderer
+#define renderer
+
+void getNormalOfPlane(double**);
+
+/** This class is for the map's static faces. **/
+class C_3DObj{
+public:
+    double* verts;             /** groups of three, 'xyzxyzxyzxyzxyz... etc.' **/
+    double* normals;           /** each face has a precomputed normal, 'xyzxyzxyzxyzxyz... etc.' **/
+    unsigned int* faces;       /** each face has three verts, '1 2 3 1 4 2 5 2 1... etc' **/
+    unsigned int numOfFaces;   /** Total number of faces. **/
+    /** Memory constructor **/
+    C_3DObj(int vertcnt, int facecnt){
+        verts = new double [vertcnt * 3];
+        normals = new double [facecnt * 3];
+        faces = new unsigned int [facecnt * 3];
+    }
+};
+
+/** Put the normalsCalc function into a class so we can safely create more than one to support multi-threaded processing. **/
+class C_nCalc{
+public:
+    void normalsCalc(C_3DObj*, int, int);
+};
+
+/** Class for the camera viewport **/
+class C_camera{
+public:
+    double cameraPos[3];        /** xyz **/
+    double cameraFocalLength;
+    double cameraAngle[2];      /** xy and yz **/
+};
+
+#endif // ggah
